c82d1759ca36b9df55379acf944c68d60ee96cfc
Joseph Kogut <joseph.kogut@gmail.com>
2017-12-20 15:00:24 -0800

    example: start glxgears automatically

--- examples/windowmanager/main.py
+++ examples/windowmanager/main.py
@@ -1,20 +1,28 @@
 from kivywm.uix.windowmanager import KivyWindowManager
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
+from kivy.logger import Logger
+from kivy.clock import Clock
+
+import subprocess
+import time
 
 class WindowManagerApp(KivyWindowManager):
     def __init__(self, *args):
         super(WindowManagerApp, self).__init__(*args)
-
         self.add_window_callback(self.add_window, name='glxgears')
 
     def add_window(self, window):
         self.root.add_widget(window)
 
     def build(self):
-        layout = GridLayout(cols=2)
+        layout = GridLayout(cols=6)
         layout.add_widget(Label(text='Kivy Window Manager'))
         return layout
+
+    def on_start(self):
+        for i in range(23):
+            p = subprocess.Popen('glxgears')
 
 if __name__ == '__main__':
     WindowManagerApp().run()
