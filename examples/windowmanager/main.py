6f73dd89ce957a6441b8ddff173a2ef9beac0d4e
Joseph Kogut <joseph.kogut@gmail.com>
2017-12-26 16:34:27 -0800

    BaseWindowManager: remove inheritance from kivy App

--- examples/windowmanager/main.py
+++ examples/windowmanager/main.py
@@ -1,3 +1,4 @@
+from kivy.app import App
 from kivywm.uix.windowmanager import KivyWindowManager
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
@@ -7,20 +8,19 @@ from kivy.clock import Clock
 import subprocess
 import time
 
-class WindowManagerApp(KivyWindowManager):
-    def __init__(self, *args):
-        super(WindowManagerApp, self).__init__(*args)
-        self.add_window_callback(self.add_window, name='glxgears')
-
-    def add_window(self, window):
-        self.root.add_widget(window)
-
+class WindowManagerApp(App):
     def build(self):
         layout = GridLayout(cols=6)
         layout.add_widget(Label(text='Kivy Window Manager'))
         return layout
 
+    def add_window(self, window):
+        self.root.add_widget(window)
+
     def on_start(self):
+        self.window_manager = KivyWindowManager()
+        self.window_manager.add_window_callback(self.add_window, name='glxgears')
+
         for i in range(23):
             p = subprocess.Popen('glxgears')
 
