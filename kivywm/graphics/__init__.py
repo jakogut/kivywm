f9a83df5c550dc39de47beebf565de78193895f0
Joseph Kogut <joseph.kogut@gmail.com>
2018-09-17 15:57:08 -0700

    graphics: initialize egl context

--- kivywm/graphics/__init__.py
+++ kivywm/graphics/__init__.py
@@ -1,7 +1,8 @@
-from kivywm.graphics.extensions import glx_init
+from kivywm.graphics.extensions import glx_init, egl_init
 from kivywm.graphics.tfp import tfp_init
 
 glx_init()
+egl_init()
 tfp_init()
 
 GL_TEXTURE_2D = 0x0DE1
