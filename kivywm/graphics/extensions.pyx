ba0be8ccd8b4044fb413f7dbbe0d5a6490df543c
Joseph Kogut <joseph.kogut@gmail.com>
2018-09-17 15:56:54 -0700

    extensions: add egl context

--- kivywm/graphics/extensions.pyx
+++ kivywm/graphics/extensions.pyx
@@ -1,5 +1,6 @@
 cdef extern from "graphics.h":
     void *glXGetProcAddress(GLubyte *)
+    void *eglGetProcAddress(const char *)
 
 cdef GLX_Context g_glx
 cdef GLX_Context *glx = &g_glx
@@ -15,4 +16,17 @@ cpdef void glx_init() except *:
     glx.glXBindTexImageEXT = <PFNGLXBINDTEXIMAGEEXTPROC>glXGetProcAddress("glXBindTexImageEXT")
     glx.glXReleaseTexImageEXT = <PFNGLXRELEASETEXIMAGEEXTPROC>glXGetProcAddress("glXReleaseTexImageEXT")
 
+cdef EGL_Context g_egl
+cdef EGL_Context *egl = &g_egl
 
+cdef EGL_Context *egl_get_context():
+    return egl
+
+cdef void egl_set_context(EGL_Context *ctx):
+    global egl
+    egl = ctx
+
+cpdef void egl_init() except *:
+    egl.eglCreateImageKHR = <PFNEGLCREATEIMAGEKHRPROC>eglGetProcAddress("eglCreateImageKHR")
+    egl.eglDestroyImageKHR = <PFNEGLDESTROYIMAGEKHRPROC>eglGetProcAddress("eglDestroyImageKHR")
+    egl.glEGLImageTargetTexture2DOES = <PFNGLEGLIMAGETARGETTEXTURE2DOESPROC>eglGetProcAddress("glEGLImageTargetTexture2DOES")
